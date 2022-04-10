3768e0368a419a7917576115b8463e34efa89cd0
funkylen <funkylen@gmail.com>
2022-04-11 01:45:29 +0300

    Добавление пагинации на страницу сайтов

--- resources/views/urls/index.blade.php
+++ resources/views/urls/index.blade.php
@@ -15,11 +15,22 @@
                 <tr>
                     <td>{{ $url->id }}</td>
                     <td><a href="{{ route('urls.show', ['url' => $url->id]) }}">{{ $url->name }}</a></td>
-                    <td>-</td>
-                    <td>-</td>
+                    <td></td>
+                    <td></td>
                 </tr>
             @endforeach
             </tbody>
         </table>
     </div>
+
+    <nav role="navigation" aria-label="Pagination Navigation">
+        <ul class="pagination">
+            <li class="page-item @if($urls->onFirstPage()) disabled @endif">
+                <a class="page-link" href="{{ $urls->previousPageUrl() }}">Previous</a>
+            </li>
+            <li class="page-item @unless($urls->hasMorePages()) disabled @endunless">
+                <a class="page-link" href="{{ $urls->nextPageUrl() }}">Next</a>
+            </li>
+        </ul>
+    </nav>
 @endsection
